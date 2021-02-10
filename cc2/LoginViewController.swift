fae649030b156f74abaea660205cec8968392bf2
ShimuraKeita <keitagsk8001@gmail.com>
2021-02-10 22:29:56 +0900

    LoginViewControllerとUIをコネクト

--- cc2/LoginViewController.swift
+++ cc2/LoginViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    //MARK: - IBOutlets
+    //Labels
+    @IBOutlet weak var emailLabelOutlet: UILabel!
+    @IBOutlet weak var passwordLabelOutlet: UILabel!
+    @IBOutlet weak var repeatPasswordLabel: UILabel!
+    
+    //TextFields
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var resetPasswordTextField: UITextField!
+    //Buttons
+    @IBOutlet weak var loginButtonOutlet: UIButton!
+    @IBOutlet weak var signUpButtonOutlet: UIButton!
+    @IBOutlet weak var resendEmailButtonOutlet: UIButton!
+    
+    //Views
+    @IBOutlet weak var repeatPasswordLineView: UIView!
+    
+    //MARK: - ViewLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    //MARK: - IBActions
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func forgotPasswordButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func resendEmailButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func signUpButtonPressed(_ sender: Any) {
+        
+    }
 }
 
