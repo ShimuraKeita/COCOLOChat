04b87b4288f847d03d7569914b9a69be392166f1
ShimuraKeita <keitagsk8001@gmail.com>
2021-02-14 07:22:32 +0900

    Realm Manager設定

---
+++ cc2/Model/RealmMessages/RealmManager.swift
@@ -0,0 +1,28 @@
+//
+//  RealmManager.swift
+//  cc2
+//
+//  Created by 志村　啓太 on 2021/02/14.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmManager {
+    
+    static let shared = RealmManager()
+    let realm = try! Realm()
+    
+    private init() {}
+    
+    func saveRealm<T: Object>(_ object: T) {
+        
+        do {
+            try realm.write {
+                realm.add(object, update: .all)
+            }
+        } catch {
+            print("Error saving realm Object", error.localizedDescription)
+        }
+    }
+}
