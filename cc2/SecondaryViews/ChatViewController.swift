9e4cde110f0bb1556d73412190c9a11bf1487271
ShimuraKeita <keitagsk8001@gmail.com>
2021-02-15 19:47:36 +0900

    作成

--- cc2/SecondaryViews/ChatViewController.swift
+++ cc2/SecondaryViews/ChatViewController.swift
@@ -76,6 +76,7 @@ class ChatViewController: MessagesViewController {
         configureCustomTitle()
 
         localChats()
+        listenForNewChats()
     }
     
     //MARK: - Configurations
@@ -187,7 +188,7 @@ class ChatViewController: MessagesViewController {
     }
     
     private func listenForNewChats() {
-        
+        FirebaseMessageListener.shared.listenForNewChats(User.currentId, collectionId: chatId, lastMessageDate: lastMessageDate())
     }
     
     private func checkForOldChats() {
@@ -219,5 +220,13 @@ class ChatViewController: MessagesViewController {
     func updateTypingIndicator(_ show: Bool) {
         
         subTitleLabel.text = show ? "Typing..." : ""
+    }
+    
+    //MARK: - Helpers
+    
+    private func lastMessageDate() -> Date {
+        
+        let lastMessageDate = allLocalMessages.last?.date ?? Date()
+        return Calendar.current.date(byAdding: .second, value: 1,to: lastMessageDate) ?? lastMessageDate
     }
 }
