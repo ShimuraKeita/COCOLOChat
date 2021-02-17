596dd58f9391b00cbf4d9e6d9f7a4b38ad4cda4c
ShimuraKeita <keitagsk8001@gmail.com>
2021-02-17 22:15:53 +0900

    channel Model, channel Cell 作成

---
+++ cc2/CustomCells/ChannelsTableViewCell.swift
@@ -0,0 +1,52 @@
+//
+//  ChannelsTableViewCell.swift
+//  cc2
+//
+//  Created by 志村　啓太 on 2021/02/17.
+//
+
+import UIKit
+
+class ChannelsTableViewCell: UITableViewCell {
+    
+    //MARK: - IBOutlet
+    
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var aboutLabel: UILabel!
+    @IBOutlet weak var memberCountLabel: UILabel!
+    @IBOutlet weak var lastMessageDateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+    func configure(channel: Channel) {
+        
+        nameLabel.text = channel.name
+        aboutLabel.text = channel.aboutChannel
+        memberCountLabel.text = "\(channel.memberIds.count) members"
+        lastMessageDateLabel = timeElapsed(channel.lastMessageDate) ?? Date()
+        lastMessageDateLabel.adjustsFontSizeToFitWidth = true
+    }
+    
+    private func setAvatar(avatarLink: String) {
+        
+        if avatarLink != "" {
+            FileStorage.downloadImage(imageUrl: avatarLink) { (avatarImage) in
+                
+                DispatchQueue.main.async {
+                    self.avatarImageView.image = avatarImage != nil ? avatarImage?.circleMasked : UIImage(named: "avatar")
+                }
+            }
+        } else {
+            self.avatarImageView.image = UIImage(named: "avatar")
+        }
+    }
+}
---
+++ cc2/Model/Channel.swift
@@ -0,0 +1,33 @@
+//
+//  Channel.swift
+//  cc2
+//
+//  Created by 志村　啓太 on 2021/02/17.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+struct Channel: Codable {
+    
+    var id = ""
+    var name = ""
+    var adminId = ""
+    var memberIds = [""]
+    var avaterLink = ""
+    var aboutChannel = ""
+    @ServerTimestamp var createdDate = Date()
+    @ServerTimestamp var lastMessageDate = Date()
+    
+    enum CodingKeys: String, CodingKeys {
+        case id
+        case name
+        case adminId
+        case memberIds
+        case avaterLink
+        case aboutChannel
+        case createdDate
+        case lastMessageDate = "date"
+    }
+}
